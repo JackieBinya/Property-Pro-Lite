6046e6d13fa8ccad814e4c0b793abb5bc5ff11e5
JackieBinya <jacquelinebinya@gmail.com>
2019-07-01 02:04:42 +0200

    [finishes # 166855271]user sign up with tdd & travis ci implemented

---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,67 @@
+import bcrypt from 'bcryptjs';
+import models from '../models';
+import generateToken from '../utils/authService';
+
+const createNewUser = (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Trim input
+  username.trim();
+  email.trim();
+  password.trim();
+
+  // salt and hash
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  const user = models.User
+    .create({
+      username,
+      email,
+      password: hash,
+    });
+
+  const token = generateToken(user.id);
+  res.status(201).json({
+    data: {
+      token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    },
+  });
+};
+
+const authUser = (req, res) => {
+  const { email, password } = req.body;
+
+  // Trim input
+  email.trim();
+  password.trim();
+
+  const user = models.User.findByEmail(email);
+
+  bcrypt.compare(password, user.password, (err, isMatch) => {
+    // res === true
+    if (err) throw err;
+
+    if (isMatch) {
+      const token = generateToken(user.id);
+      return res.status(200).json({
+        data: {
+          token,
+          user: {
+            username: user.username,
+            email: user.email,
+          },
+        },
+      });
+    }
+
+    if (!isMatch) return res.status(401).json({ msg: 'Authentification failed incorrect password!' });
+  });
+};
+
+
+export { createNewUser, authUser };
---
+++ server/src/v1/middlewares/verify.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import models from '../models';
+
+const verifyNewUser = (req, res, next) => {
+  const { email } = req.body;
+
+  const result = models.User.findByEmail(email);
+  if (result) return res.status(400).json({ msg: 'Your email is already registered in the app, you are only allowed to have one account.' });
+  next();
+};
+
+const verifyExistingUser = (req, res, next) => {
+  const { email } = req.body;
+
+  const result = models.User.findByEmail(email);
+  if (!result) return res.status(400).json({ msg: 'Please sign up to continue, if already signed up email you provided is incorrect. Please try again.' });
+  next();
+};
+
+const verifyAuthUser = (req, res, next) => {
+  // Note how you grab token from req.header();
+  const token = req.header('x-auth-token');
+
+  if (!token) return res.status(401).json({ msg: 'No token access denied' });
+
+  jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+    if (err) return res.status(401).json({ msg: 'No authorisation, token invalid!' });
+    req.decoded = decoded;
+    next();
+  });
+};
+
+export { verifyNewUser, verifyExistingUser, verifyAuthUser };
