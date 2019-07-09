6f09db81d87bdf3d8be8d5bd483d4911cbabba15
JackieBinya <jacquelinebinya@gmail.com>
2019-07-09 06:45:41 +0200

    bug(ui): modify signup, login, signup.js
      - add a submit-btn class to submit buttons on the signup and login forms
      - add a script to signup.js that ensures that after clicking submit button user is redirected to home page
    
      [finishes #167159263]

--- signUp.js
+++ signUp.js
@@ -3,3 +3,9 @@ const displayError = document.querySelector('.display-msg')
 document.querySelector('.del-msg').addEventListener('click', e => {
     displayError.style.display = 'none';
 });
+
+
+document.querySelector('.submit-btn').addEventListener('click', e => {
+    window.location.href='./index.html';
+});
+
